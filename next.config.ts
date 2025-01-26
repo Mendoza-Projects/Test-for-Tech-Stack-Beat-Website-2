af5dc86490514d6af8c40e1fadabbf47791a9c84
Mendoza <gbenlemon@gmail.com>
2025-01-25 18:12:42 -0600

    maybe made the links work

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  trailingSlash: true,  // Ensures paths have a trailing slash
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'img.youtube.com',
         pathname: '/vi/*/0.jpg',
+        
       },
+      
     ],
   },
 };
